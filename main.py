697f501dfbddb3bd95f4c8569eb928b8c7f25931
Elizabete Pliene <epliene7@gmail.com>
2024-01-15 15:43:23 +0200

    Update main.py
    Mēneša ielaksu aprēķins

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 
-df = yf.download("AAPL",
-    start="2010-01-01",
-    end="2020-12-31",
+#getting user input
+stock = input("Lūdzu ievadiet vēlamas akcijas tickeri (piem., AAPL): ")
+start_date = input("Ievadiet invsetīciju sākuma datumu (formā yyyy-mm-dd): ")
+end_date = input("Ievadiet beigu datumu (formā yyyy-mm-dd): ")
+freq = input("Ievadiet 1, ja vēlatie iemaksas veikt reizi gadā, 2, ja vēlaties tās veikt reizi mēnesī: ")
+cap = input("Ievadiet sākuma kapitāla summu: ")
+sum = input("Ievadiet kādu summu vēlaties iemaksāt katrā reizē: ")
+
+# iemaksa reizi mēnesī
+df = yf.download(stock,
+    start=start_date,
+    end=end_date,
     progress=False)
-df = df.loc[:, ["Adj Close"]]
+df['Date'] = df.index
+df_first_of_month = df.groupby(df['Date'].dt.to_period("M")).first()
+df_last_of_month = df.groupby(df['Date'].dt.to_period("M")).last()
+print(df_first_of_month['Open'])
 
-df["simple_rtn"] = df["Adj Close"].pct_change()
-df["log_rtn"] = np.log(df["Adj Close"]/df["Adj Close"].shift(1))
+portfelis = float(cap)/float(df_first_of_month.iloc[0]['Open'])
 
-print(df)
-
-def realized_volatility(x):
-    return np.sqrt(np.sum(x**2))
-
-df_rv = (
-    df.groupby(pd.Grouper(freq="M"))
-    .apply(realized_volatility)
-    .rename(columns={"log_rtn": "rv"})
-)
-
-df_rv.rv = df_rv["rv"] * np.sqrt(12)
-
-print(df_rv)
+for i in range (1, len(df_first_of_month)):
+    portfelis=portfelis+float(sum)/df_first_of_month.iloc[i]['Open']
+protfela_vertiba=portfelis*float(df_last_of_month.iloc[len(df_last_of_month)-1]['Open'])
+pelna=protfela_vertiba-float(cap)-(float(sum)*int(len(df_first_of_month)))
+print(portfelis)
+print(protfela_vertiba)
+print(pelna)
